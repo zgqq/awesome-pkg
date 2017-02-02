f9fb73a9e7d46addb58e3a6bdaab21cdbb4ced9a
zgqq <zgqjava@gmail.com>
2017-02-02 03:30:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Awesome-pkg
+This project aims to collect all kinds of useful packages on various systems.
---
+++ archlinux.sh
@@ -0,0 +1,14 @@
+# Shell
+pacman -S git hub zsh neovim tmux fzf fasd the_silver_searcher thefuck
+
+# Go
+pacman -S go
+
+# Aur
+mkdir $HOME/Package
+cd $HOME/Package
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si && cd $HOME/Package
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt && makepkg -si
